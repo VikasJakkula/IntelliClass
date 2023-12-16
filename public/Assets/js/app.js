be4070e0f4145ecfd047e8a192bcdff890995ec5
Sohail Akthar <sohailakther2002@gmail.com>
2023-12-17 05:07:44 +0530

    Completed till video-12 & Fixed some erros

--- public/Assets/js/app.js
+++ public/Assets/js/app.js
@@ -1,8 +1,32 @@
-var Myapp = (function(){
+var MyApp = (function(){
+
+    var socket = null;
+    var user_id = "";
+    var meeting_id = "";
 
     function init(uid,mid){
-        alert("From App js");
+      user_id = uid;
+      meeting_id = mid;
+      event_process_for_signaling_server();
+    }
 
+
+    function event_process_for_signaling_server() {
+      socket = io.connect()
+      socket.on("connect", () => {
+        if(socket.connected) {
+          if(user_id != "" && meeting_id != "") {
+            socket.emit("userconnect", {
+              displayName: user_id,
+              meetingid: meeting_id
+            })
+          }
+        }
+      });
+
+      socket.on("inform_others_about_me", function(data){
+        addUser(data.other_user_id, data.connId);
+      })
     }
 
 
